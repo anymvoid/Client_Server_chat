01ce668445a8c56f8a6abd12e29d478043e65dd3
Enea Hoxha <eneahoxha05@hotmail.com>
2025-04-26 17:23:18 +0200

    Modifiche al  file Server.py  e client1 /2 dopo  aver creato e testato i file dentro la cartella  prova

--- Client_2.py
+++ Client_2.py
@@ -1,21 +1,37 @@
 import socket
-import threading
+from threading import Thread
+import os
 
-def receive(sock):
-    while True:
-        try:
-            msg = sock.recv(1024).decode()
-            if msg:
-                print(msg)
-        except:
-            break
+class Client:
+    def __init__(self, HOST, PORT):
+        self.socket = socket.socket()
+        self.socket.connect((HOST, PORT))
+        self.talk_to_server()
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1", 12345))
+    def talk_to_server(self):
+        Thread(target=self.receive_message).start()
+        self.send_message()
 
-thread = threading.Thread(target=receive, args=(client,))
-thread.start()
+    def send_message(self):
+        while True:
+            try:
+                client_message = input("")
+                self.socket.send(client_message.encode())
+            except:
+                self.socket.close()
+                os._exit(0)
 
-while True:
-    msg = input()
-    client.send(msg.encode())
+    def receive_message(self):
+        while True:
+            try:
+                server_message = self.socket.recv(1024).decode()
+                if not server_message:
+                    print("Server disconnesso.")
+                    self.socket.close()
+                    os._exit(0)
+                print("\033[1;32;40m" + server_message + "\033[0m")
+            except:
+                self.socket.close()
+                os._exit(0)
+
+Client('127.0.0.1', 7632)
--- Server.py
+++ Server.py
@@ -1,41 +1,62 @@
 import socket
-import threading
+from threading import Thread
+import os
 
-clients = []
+class Server:
+    def __init__(self, HOST, PORT):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((HOST, PORT))
+        self.socket.listen()
+        print('Server in attesa di connessioni...')
+        self.clients = []
 
-def handle_client(conn, addr):
-    print(f"{addr} connesso.")
-    while True:
-        try:
-            msg = conn.recv(1024).decode()
-            if msg:
-                print(f"{addr}: {msg}")
-                broadcast(f"{addr}: {msg}", conn)
-            else:
-                break
-        except:
-            break
-    conn.close()
-    clients.remove(conn)
-    print(f"{addr} disconnesso.")
+        self.accept_clients()
 
-def broadcast(msg, sender_conn):
-    for client in clients:
-        if client != sender_conn:
+    def accept_clients(self):
+        while True:
+            client_socket, address = self.socket.accept()
+            print(f"Connesso con {address}")
+            self.clients.append(client_socket)
+            Thread(target=self.talk_to_client, args=(client_socket, address)).start()
+
+    def talk_to_client(self, client_socket, address):
+        Thread(target=self.receive_message, args=(client_socket, address)).start()
+        self.send_message(client_socket, address)
+
+    def send_message(self, client_socket, address):
+        while True:
             try:
-                client.send(msg.encode())
+                message = input("")
+                self.broadcast(f"Server: {message}")
             except:
-                client.close()
-                clients.remove(client)
+                client_socket.close()
+                self.clients.remove(client_socket)
+                break
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 12345))
-server.listen()
+    def receive_message(self, client_socket, address):
+        while True:
+            try:
+                message = client_socket.recv(1024).decode()
+                if not message or message.strip().lower() == "exit":
+                    print(f"{address} disconnesso.")
+                    client_socket.close()
+                    self.clients.remove(client_socket)
+                    break
+                print(f"\033[1;31;40m{address}: {message}\033[0m")
+                self.broadcast(f"{address}: {message}", sender=client_socket)
+            except:
+                client_socket.close()
+                if client_socket in self.clients:
+                    self.clients.remove(client_socket)
+                break
 
-print("Server multiclient avviato...")
+    def broadcast(self, message, sender=None):
+        for client in self.clients:
+            if client != sender:
+                try:
+                    client.send(message.encode())
+                except:
+                    client.close()
+                    self.clients.remove(client)
 
-while True:
-    conn, addr = server.accept()
-    clients.append(conn)
-    thread = threading.Thread(target=handle_client, args=(conn, addr))
-    thread.start()
+Server('127.0.0.1', 7632)
